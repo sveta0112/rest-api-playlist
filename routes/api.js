4057ea02585d4b8bec21a2d1c51013aec49b6aad
sveta0112 <sveta0112@hotmail.com>
2019-09-07 22:09:19 -0700

    UPDATE router handling

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 //get a list of ninjas from DB
 router.get('/ninjas', function(req, res, next) {
-    res.send({type: 'GET'})
+    res.send(req.body)
 
    //res.end() --> to stop spiining in browser
 });
@@ -22,7 +22,14 @@ router.post('/ninjas', function(req, res, next) {
 
 //update an existing ninja in DB
 router.put('/ninjas/:id', function(req, res, next) {
-    res.send({type: 'UPDATE'})
+    let id = req.params.id;
+    //it gets update record in mongoDb, but it still showa outdated record
+    Ninja.findByIdAndUpdate({_id: id}, req.body).then(() => {
+        //in order to see update recors , need to use findOne() method
+        Ninja.findOne({_id: id}).then(ninjaObj => {
+            res.send(ninjaObj);
+        });
+    });
 });
 
 //delete an existing ninja from DB
@@ -30,8 +37,7 @@ router.delete('/ninjas/:id', function(req, res, next) {
     //console.log(req.params.id);
     Ninja.findByIdAndRemove({_id: req.params.id}).then(ninjaObj => {
         res.send(ninjaObj);
-    })
-
+    });
 });
 
 
